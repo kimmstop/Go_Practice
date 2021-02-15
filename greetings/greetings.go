b10c2a2710619b1349fa70023fdfe328ba6ca988
kimmstop <kimmstop@gmail.com>
2021-02-15 10:43:26 +0000

    Return random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -3,12 +3,28 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
+}
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome",
+		"Great to see you, %v",
+		"Haaaaa well done %v",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
